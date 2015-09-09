815310a50312d01d4cc502df56fcf01689b02dab
indosandi <indosandi@gmail.com>
2015-09-09 16:48:17 -0500

    correct integration for both imag and real

--- 3d/rhoMtoAmnoBess.m
+++ 3d/rhoMtoAmnoBess.m
@@ -10,14 +10,14 @@ for iq=1:nringsq
     for l=0:lmax
     for m=-l:l
         temp=zeros(nrings,1);
-        %tempx=zeros(nrings,1);
+        tempx=zeros(nrings,1);
         for ir=1:nrings
             r=(ir-1)*dr;
             q=(iq-1)*dq;
-            temp(ir)=4*pi*rhoLM(ir,l+1,l+m+1)*spbs(l,q*r*2*pi)*(j)^larray(l+1)*r^2;
-            %tempx(ir)=r; 
+            temp(ir)=4*pi*rhoLM(ir,l+1,l+m+1)*spbs(l,q*r*2*pi)*(-j)^larray(l+1)*r^2;
+            tempx(ir)=r; 
         end
-        Almq(iq,l+1,m+l+1)=trapz(temp);
+        Almq(iq,l+1,m+l+1)=trapz(tempx,temp);
     end
     end
 end
